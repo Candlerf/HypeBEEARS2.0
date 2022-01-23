71b0bbd72bc44a62a03d352a2d3c427d24cddc73
Candlerf <96502267+Candlerf@users.noreply.github.com>
2022-01-23 15:07:55 -0500

    Add files via upload

---
+++ precache-manifest.7cc25139c94d8550e394f91e82eaa363.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29d610ecf32d4dcb5fae14234aa48c3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faf1b9b4e3b2bbca5d16",
+    "url": "/static/css/main.de453e59.chunk.css"
+  },
+  {
+    "revision": "5f49c52508801ac26426",
+    "url": "/static/js/2.19a2e286.chunk.js"
+  },
+  {
+    "revision": "306907fccee50102c62f",
+    "url": "/static/js/3.4896f7ca.chunk.js"
+  },
+  {
+    "revision": "faf1b9b4e3b2bbca5d16",
+    "url": "/static/js/main.640b97f2.chunk.js"
+  },
+  {
+    "revision": "5a497db0d7d0bf2e4fcb",
+    "url": "/static/js/runtime~main.b3e4c5be.js"
+  },
+  {
+    "revision": "e63562614cbdc491e4d374f97c513b82",
+    "url": "/static/media/font.e6356261.woff"
+  }
+]);
